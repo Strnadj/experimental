c0e107d5a2c05ba7feb053775ec664ed6038dc2c
Strnadj <jan.strnadek@gmail.com>
2016-01-25 10:54:37 +0100

    Test

--- ext/tbb_ruby/extconf.rb
+++ ext/tbb_ruby/extconf.rb
@@ -7,17 +7,11 @@ require 'byebug'
 $CFLAGS << ' --std=c++11'
 
 # Process arguments
-# dir_config('tbb')
-
-
-LIB_DIRS = ["/usr/local/Cellar/tbb/4.4-20150728/lib/"]
-HEADER_DIRS    = ["/usr/local/Cellar/tbb/4.4-20150728/include/"]
-
-dir_config('.', HEADER_DIRS, LIB_DIRS)
+dir_config('tbb')
 
 # Check library
-# unless have_library('tbb')
-#  abort 'Please install TBB library!'
-# end
+unless have_library('tbb')
+  abort 'Please install TBB library!'
+end
 
 create_makefile('tbb_ruby/tbb_ruby')
